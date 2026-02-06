a3cd7540c78335f1ae527e809e03ad784437859d
loganv308 <loganv308@gmail.com>
2026-02-05 21:10:47 -0600

    Add /bin/bash to setupTailscale, updater, genNASCredentials, and templateScript scripts

---
+++ Linux Scripts/Apt/updater.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+apt update -y && apt full-upgrade -y && apt dist-upgrade -y
--- Linux Scripts/Tailscale/setupTailscale.sh
+++ Linux Scripts/Tailscale/setupTailscale.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Add keyring and repository for Tailscale
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/plucky.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/plucky.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
